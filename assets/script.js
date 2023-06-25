4b1f8db69a8a9e2b6e585b20ab8d3804bacf800b
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-06-25 14:37:59 -0500

    added pseudo code and blank functions

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,48 @@
+var
+var
+var
+var
+var
+var
+var
+var questionsList[
+    {questionText: "Question text",
+    answer1: "1",
+    answer2: "2",
+    answer3: "3",
+    answer4: "4",
+    correctAnswer: "Correct Answer Test"
+},
+    {questionText: "Question text",
+    answer1: "1",
+    answer2: "2",
+    answer3: "3",
+    answer4: "4",
+    correctAnswer: "Correct Answer Test"
+},
+    {questionText: "Question text",
+    answer1: "1",
+    answer2: "2",
+    answer3: "3",
+    answer4: "4",
+    correctAnswer: "Correct Answer Test"
+}
+]
+
+function getChoices() //push answers variable in questions object into each button
+
+function getQuestion() //adds questions text from object into page
+
+function selectAnswer () //used when an answer button is selected
+
+function saveScore() //used at the end to save score into high scores
+
+function startQuiz() //used when start button is clicked
+
+function startTimer() //begins decrementing the timer
+
+function stopTimer() //ends deccrementing the timer, saves the time left as a string variable
+
 //start button pressed
   //h1 is addClass("hidden")
   //start-screen-text is addClass("hidden")
@@ -10,16 +55,23 @@
   //answer[2] text is pulled into answer-3-button
   //answer[3] text is pulled into answer-4-button
 
-//an answer is selected
+//an answer button is selected
 
 //it is right or wrong
-  //right if button pressed text is equal to 'answer' variable in "question" object
-//next question information is pulled from array
+ //right if button pressed text is equal to 'answer' variable in "question" object
+ //if correct
+  //pull next question
+ //else
+  //decrement timer 10
+  //pull next question
+//if no more questions, then display quiz-end-screen
+  //addClass("hidden") to quiz-container
+  //removeClass('hidden") to quiz-end-screen
 
-//once all questions are answered
+
 
 //quiz-end-screen will display
 
-//final score is displayed
-
 //user submits initials into High Scores
+  //initials are taken as a string and concact with strignified timer value then saved into local storage
+  //when clicking high scores link at top, addClass("hidden") to all other fields except high-score-screen
