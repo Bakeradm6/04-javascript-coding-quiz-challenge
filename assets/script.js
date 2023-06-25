8cce50203c233556a0cab00063ae4189a5f7b5b2
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-06-25 14:10:30 -0500

    added high score screen

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,25 @@
-console.log("Test")
+//start button pressed
+  //h1 is addClass("hidden")
+  //start-screen-text is addClass("hidden")
+  //quiz-container is removeClass("hidden")
+
+// first question generates
+  //questionText is pulled from object and added to #question-text
+  //answer[0] text is pulled into answer-1-button
+  //answer[1] text is pulled into answer-2-button
+  //answer[2] text is pulled into answer-3-button
+  //answer[3] text is pulled into answer-4-button
+
+//an answer is selected
+
+//it is right or wrong
+  //right if button pressed text is equal to 'answer' variable in "question" object
+//next question information is pulled from array
+
+//once all questions are answered
+
+//quiz-end-screen will display
+
+//final score is displayed
+
+//user submits initials into High Scores
